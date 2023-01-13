b56f6c8f81b398c3b398c12228ca4a0546fa7f6c
daniela marques <dd978452@gmail.com>
2023-01-13 13:24:13 -0300

    Adicionar arquivos de projeto.

---
+++ exemplo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            x = 5;
+            y = 2 * x;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            //
+
+           double b, B, h, area;
+
+            b = 6.0;
+            B = 8.0;
+            h = 5.0;
+
+            area = (b + B) / 2.0 * h;
+
+            //
+
+            Console.WriteLine(area);
+
+            int c, d;
+            double resultado;
+            c = 5;
+            d = 2;
+
+            resultado = c / d;
+
+            Console.WriteLine(resultado);
+
+            //
+
+            double e;
+            int f;
+
+            e = 5.0;
+            f = (int) e;
+
+
+            Console.WriteLine(f);
+        }
+    }
+
+}
